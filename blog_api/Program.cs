d96ee5f5c65059c5a178d7b49c395ca57e88dc23
KenGao1088 <ken.gao@unifiedincentives.com>
2023-03-20 16:17:38 +0800

    add BlogService to middleware

--- blog_api/Program.cs
+++ blog_api/Program.cs
@@ -1,3 +1,6 @@
+using blog_api.Service;
+using blog_api.Service.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IBlogService, BlogService>();
 
 var app = builder.Build();
 
