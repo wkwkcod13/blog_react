51be6227d7b02c6e51ca23c2d99063106e8a25f4
KenGao1088 <ken.gao@unifiedincentives.com>
2023-03-20 09:40:09 +0800

    加入專案檔案。

---
+++ blog_api/Controllers/BlogController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace blog_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BlogController : ControllerBase
+    {
+        [HttpHead]
+        public IActionResult GetHead()
+        {
+            return new JsonResult(null);
+        }
+
+        [HttpGet]
+        public IActionResult GetList()
+        {
+            return new JsonResult(null);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetDetail(string id)
+        {
+            return new JsonResult(null);
+        }
+
+        [HttpPost()]
+        public IActionResult CreateDetail(object param)
+        {
+            return new JsonResult(null);
+        }
+
+        [HttpPut()]
+        public IActionResult UpdateDetail(object param)
+        {
+            return new JsonResult(null);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult PatchDetail(string id, object param)
+        {
+            return new JsonResult(null);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            return new JsonResult(null);
+        }
+    }
+}
