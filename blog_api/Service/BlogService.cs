3b859e0296d906f574975c252541b266c97e753d
KenGao1088 <ken.gao@unifiedincentives.com>
2023-03-20 16:10:29 +0800

    add Service.BlogService

---
+++ blog_api/Service/BlogService.cs
@@ -0,0 +1,27 @@
+﻿using blog_api.Models;
+using blog_api.Service.Interface;
+
+namespace blog_api.Service
+{
+    public class BlogService : IBlogService
+    {
+        private readonly BlogList<IBlog> _blogList;
+        public BlogService()
+        {
+            _blogList = new BlogList<IBlog>();
+        }
+
+        public IBlog? GetBlogByIndex(int index)
+        {
+            return (IBlog?)_blogList[index];
+        }
+        public int CreateBlog(IBlog blog)
+        {
+            return _blogList.Add(blog);
+        }
+        public BlogList<IBlog> GetBlogList()
+        {
+            return _blogList;
+        }
+    }
+}
---
+++ blog_api/Service/Interface/IBlogService.cs
@@ -0,0 +1,11 @@
+﻿using blog_api.Models;
+
+namespace blog_api.Service.Interface
+{
+    public interface IBlogService
+    {
+        int CreateBlog(IBlog blog);
+        IBlog? GetBlogByIndex(int index);
+        BlogList<IBlog> GetBlogList();
+    }
+}
