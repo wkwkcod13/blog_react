e8a522a65adb1643f75f0fb0d66d08c4e749b5ce
KenGao1088 <ken.gao@unifiedincentives.com>
2023-03-21 17:29:23 +0800

    add MyComponent , there are some bug is waiting to fixed.

--- blog_react/src/components/Home.js
+++ blog_react/src/components/Home.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import MyComponent from './MyComponent';
 
 export class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { forecasts: [], loading: true };
+    }
+
     render() {
-        return (<div>home</div>);
+        return (
+            <div>
+                <MyComponent></MyComponent>
+            </div>
+        );
     }
 }
