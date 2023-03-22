a97e38aa73d1692b1ea7ed5567d7d87cc5fd3557
KenGao1088 <ken.gao@unifiedincentives.com>
2023-03-22 11:42:21 +0800

    add PackageReference Microsoft.AspNetCore.Cors

--- blog_api/Program.cs
+++ blog_api/Program.cs
@@ -1,12 +1,20 @@
 using blog_api.Service;
 using blog_api.Service.Interface;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+    {
+        policy.WithOrigins("https://localhost:3000");
+    });
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IBlogService, BlogService>();
@@ -21,7 +29,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
 
 app.MapControllers();
