583f5ae0eb4d336c493df33c6227bb20cf95fac9
KenGao1088 <ken.gao@unifiedincentives.com>
2023-03-27 13:34:36 +0800

    .

---
+++ blog_api/Service/TestPdfService.cs
@@ -0,0 +1,25 @@
+﻿using iText;
+using iText.Kernel.Pdf;
+using iText.Html2pdf;
+
+namespace blog_api.Service
+{
+    public class TestPdfService
+    {
+        public TestPdfService()
+        {
+
+        }
+
+        public void PdfReader()
+        {
+            PdfDocument pdf = new PdfDocument(new PdfReader(""));
+            PdfReader reader = new PdfReader("", new ReaderProperties());
+        }
+
+        public void Pdf2Html()
+        {
+            PdfDocument pdf = new PdfDocument(new PdfReader(""));
+        }
+    }
+}
