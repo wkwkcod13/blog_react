fa8cf3680cb256fead8d4c60eec62241d2e5dc2b
KenGao1088 <ken.gao@unifiedincentives.com>
2023-03-27 13:45:05 +0800

    .

--- blog_react/src/components/PostEdit.js
+++ blog_react/src/components/PostEdit.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 export class PostEdit extends Component {
     render() {
-        return (<div>PostEdit</div>);
+        return (
+            <div>
+                <span>
+                    PostEdit
+                </span>
+                <CKEditor
+                    editor={ClassicEditor}
+                    data="<p>Hello from CKEditor 5!</p>"
+                />
+            </div>);
     }
 }
