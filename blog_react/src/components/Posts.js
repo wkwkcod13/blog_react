0ee5a8734fe95d7b62200305680959f012bd855a
KenGao1088 <ken.gao@unifiedincentives.com>
2023-03-28 17:12:40 +0800

    call api method and show post list.

--- blog_react/src/components/Posts.js
+++ blog_react/src/components/Posts.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import ApiRoutes from '../ApiRoutes';
 
 export class Posts extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            events: []
+        };
+        this.fetchPostList();
+    }
+
+    fetchPostList() {
+        fetch(ApiRoutes.ApiRoot + '/blog', { method: "GET" })
+            .then(async (res) => {
+                let json = await res.json()
+                console.log(json);
+                this.setState({
+                    events: json
+                });
+            });
+    }
+
     render() {
         return (
             <div>
                 <span>Posts</span>
+                <ul>
+                    {
+                        this.state.events.map((item, index) => (
+                            <li key={index} id={item.blogId}>
+                                <a href={ './posts/' + index}>
+                                    {item.title} {item.blogId}
+                                </a>
+                            </li>
+                        ))
+                    }
+                </ul>
+
             </div>
         );
     }
