3d063491cc1c7d58f87d1dbcc6ef3eb13eb0fad6
KenGao1088 <ken.gao@unifiedincentives.com>
2023-03-31 10:18:30 +0800

    add Custom handlers for Start Up class

--- blog_api/Program.cs
+++ blog_api/Program.cs
@@ -1,11 +1,4 @@
-using blog_api.Helper;
-using blog_api.Models;
-using blog_api.Service;
-using blog_api.Service.Interface;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Text;
+using blog_api.Handler;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
@@ -16,88 +9,17 @@ var encryKey = builder.Configuration.GetValue<string>("JwtSettings:EncryKey");
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
-    {
-        policy.WithOrigins("https://localhost:3000");
-    });
-});
+builder.Services.AddCors(CustomCors.DefaultCorsOptions(MyAllowSpecificOrigins));
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddAuthentication(option =>
-{
-    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = false;
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signKey)),
-        TokenDecryptionKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(encryKey))
-    };
-});
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("RequireAdminRole", policy =>
-    {
-        policy.RequireRole("admin");
-    });
-    options.AddPolicy("RequireUserRole", policy =>
-    {
-        policy.RequireRole("user");
-    });
-});
+builder.Services.AddAuthentication(CustomAuthentication.DefaultAuthenticationOptions)
+    .AddJwtBearer(CustomAuthentication.DefaultBearerOptions(signKey, encryKey));
 
-builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<JwtHelper>();
-builder.Services.AddSingleton<IBlogService, BlogService>();
-builder.Services.AddSingleton<ICalendarService, CalendarService>();
-builder.Services.AddSingleton<ICalendarFactory, CalendarFactory>();
-
-builder.Services.AddSwaggerGen(options =>
-{
-    var securityScheme = new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        BearerFormat = "JWT",
-        Scheme = "bearer",
-        Description = "Enter your JWT token",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-    };
-    options.AddSecurityDefinition("Bearer", securityScheme);
-    var securityRequirement = new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        }
-    };
-    options.AddSecurityRequirement(securityRequirement);
-});
+builder.Services.AddAuthorization(CustomAuthorization.DefaultAuthorizationOptions);
+builder.Services.AddCustomServiceCollection();
+builder.Services.AddSwaggerGen(CustomSwaggerGen.DefaultSwaggerGenOptions);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
 
 app.UseSwagger();
 app.UseSwaggerUI();
