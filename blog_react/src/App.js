22f7718ecbe7eb6bf2a216d200fbca2651535475
KenGao1088 <ken.gao@unifiedincentives.com>
2023-04-11 16:25:33 +0800

    add bootstrap navbar

--- blog_react/src/App.js
+++ blog_react/src/App.js
@@ -2,6 +2,7 @@ import './input.css';
 import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
+import AppNav from './AppNav';
 
 export default class App extends Component {
     static displayName = App.name;
@@ -17,7 +18,15 @@ export default class App extends Component {
                 {
                     AppRoutes.map((route, index) => {
                         const { element, ...rest } = route;
-                        return <Route key={index} {...rest} element={element}></Route>
+                        return (
+                            <Route key={index} {...rest}
+                                element={
+                                    <div>
+                                        <AppNav></AppNav>
+                                        {element}
+                                    </div>}
+                            ></Route>
+                        )
                     })
                 }
             </Routes>
