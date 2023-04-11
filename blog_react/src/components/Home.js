0be9015e7a6c5c003b4843a33d99e6b671855f37
KenGao1088 <ken.gao@unifiedincentives.com>
2023-04-11 16:24:06 +0800

    add check login method

--- blog_react/src/components/Home.js
+++ blog_react/src/components/Home.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import MyComponent from './MyComponent';
+import fnlogin from './function/fnLogin';
+import jwtToken from './jwtToken';
+import csrfToken from './csrfToken';
+import ApiRoutes from '../ApiRoutes';
 
 export class Home extends Component {
     constructor(props) {
         super(props);
         this.state = { forecasts: [], loading: true };
+        this.initAsync();
+    }
+    async initAsync() {
+        await fetch(ApiRoutes.ApiRoot + '/Blog', {
+            method: "HEAD",
+            headers: {
+                'Authorization': `Bearer ${jwtToken.fnTokenGet()}`
+            }
+        }).then((res) => {
+            //console.log(res);
+        }).catch(error => {
+            jwtToken.fnTokenClear();
+        })
+        const token = jwtToken.fnTokenGet();
+        //console.log(token);
+        if (token === null) {
+            await fnlogin('qwe', 'ewq');
+            //console.log("after:" + jwtToken.fnTokenGet());
+        }
+        
     }
 
     render() {
         return (
             <div>
-                <MyComponent></MyComponent>
             </div>
         );
     }
